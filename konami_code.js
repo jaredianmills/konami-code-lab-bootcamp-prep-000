c62376604847d3e70cb844489ac3e874193b4888
jaredianmills <jaredianmills@gmail.com>
2018-04-25 18:18:56 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,10 +4,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 function init() {
   const body = document.querySelector('body');
   var i = 0;
-  var keyPress = body.addEventListener('keydown', function(e) {
-    keyPress = parseInt(e.which || e.detail);
-  });
   body.addEventListener('keydown', function(e) {
+    var keyPress = parseInt(e.which || e.detail);
     if (keyPress === code[i]) {
       i++;
         if (i === code.length) {
